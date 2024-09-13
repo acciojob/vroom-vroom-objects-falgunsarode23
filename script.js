209035ba543ce00265e478f78107b7b006d43c4d
Falgun Sarode <147540719+falgunsarode23@users.noreply.github.com>
2024-09-14 00:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.make = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+	return this.make + " " + this.model;
+}
+function SportCar(make, model , topSpeed){
+	car.call(this, make , model);
+	this.topSpeed = topspeed;
+}
+
+SportCar.Prototype = Object.create(Car.prototype);
+
+SportCar.Prototype.getTopSpeed = function(){
+	return this.topSpeed;
+}
+
 
 // Do not change the code below
 window.Car = Car;
